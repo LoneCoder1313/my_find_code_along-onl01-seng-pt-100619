fb3a5f68319c94cebd56a1b0a660efe38ff07791
Ricky dildy <rdildy92@gmail.com>
2019-10-30 01:27:39 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -14,4 +14,14 @@ def my_find(collection)
     yield(1)
     i = i + 1 
   end 
+end  
+
+def my_find(collection)
+  i = 0 
+  while i < collection.length 
+    if yield(collection[i])
+      return collection[i]
+    end 
+    i = i + 1 
+  end 
 end 
